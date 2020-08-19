455f44755213176d4a7abf1946425d72f2404906
Sultan <m.sultan338@gmail.com>
2020-08-19 23:20:03 +0500

    wow

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import {
 } from './message.js';
 export default function App() {
   let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(true);
   return ( <
-    div >
+    div className = "box" >
     <
+    h1 > Have a good {
+      isMorning ? 'Morning' : 'Night'
+    }! < /h1>
+
     Message counter = {
       count
     }
